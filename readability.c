100a781784064f2be8235ecee316aa62aa77ef27
DedPip <125252491+DedPip@users.noreply.github.com>
2025-04-04 18:07:07 +0330

    Add text input & grading func in main

--- readability.c
+++ readability.c
@@ -1,6 +1,7 @@
 #include "./libs/cs50.h"
 #include <ctype.h>
 #include <stdio.h>
+#include <math.h>
 #include <string.h>
 
 
@@ -13,14 +14,41 @@ const int asciiOfDot = 46, asciiOfBlank = 32, asciiOfComma = 44, asciiOfQuestion
 int main(void)
 {
     // string text = "One fish. Two fish. Red fish. Blue fish.";
-    string text = "Would you like them here or there? I would not like them here or there. I would not like them anywhere.";
+
+    string text = get_string("Text: ");
     
     int textLength = strlen(text);
+    int numberOfLetters = count_letters(text);
+    printf("%i\n", numberOfLetters);
     int numberOfWords = count_words(text, 0, textLength);
     printf("%i\n", numberOfWords);
 
-    int numberOfSentencesss = count_sentences(text);
-    printf("%i\n", numberOfSentencesss);
+    int numberOfSentences = count_sentences(text);
+    printf("%i\n", numberOfSentences);
+    
+    // l is the average number of letters per 100 words
+    float l = ((float) (numberOfLetters * 100)) / numberOfWords;
+    printf("%f\n", l);
+    //s is the average number of sentences per 100 words
+    float s = ((float) (numberOfSentences * 100)) / numberOfWords;
+    printf("%f\n", s);
+
+    float index = (0.0588 * l) - (0.296 * s) - 15.8;
+    printf("%f\n", index);
+    int index_rounded = round(index);
+    printf("%i\n", index_rounded);
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index_rounded);
+    }
 }
 
 int count_sentences(string text)
