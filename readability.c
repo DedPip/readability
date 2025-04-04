8ac626f871b1ad171759248f0b7c3a6180112680
DedPip <125252491+DedPip@users.noreply.github.com>
2025-04-04 12:45:56 +0330

    Add count_words func

--- readability.c
+++ readability.c
@@ -4,27 +4,52 @@
 #include <ctype.h>
 
 int count_letters(string text);
+int count_words(string text);
 
 int main(void)
 {
-    string text = "One fish. Two fish. Red fish. Blue fish.";
-    // string text = "Would you like them here or there? I would not like them here or there. I would not like them anywhere.";
+    // string text = "One fish. Two fish. Red fish. Blue fish.";
+    string text = "Would you like them here or there? I would not like them here or there. I would not like them anywhere.";
 
-    int i = count_letters(text);
-
+    int i = count_words(text);
     printf("%i\n", i);
 
 }
 
+int count_words(string text)
+{
+    int asciiOfBlank = 32;
+    int numberOfWords = 0;
+    for (int i = 0, textLength = strlen(text); i < textLength; i++)
+    {
+        if(isalpha(text[i]))
+        {
+            int j = i;
+            while ((int) text[j] != asciiOfBlank)
+            {
+                j++;
+            }
+            i = j;
+            numberOfWords++;
+            continue;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return numberOfWords;
+}
+
 int count_letters(string text)
 {
-    int letters = 0;
+    int numberOfLetters = 0;
     for (int i = 0, textLength = strlen(text); i < textLength; i++)
     {
         if (isalpha(text[i]))
         {
-            letters++;
+            numberOfLetters++;
         }
     }
-    return letters;
+    return numberOfLetters;
 }
