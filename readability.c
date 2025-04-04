6f2573ea8b10cab9ced76e138f0940f5a75100a1
DedPip <125252491+DedPip@users.noreply.github.com>
2025-04-04 16:39:57 +0330

    Polish & add count_sentences func

--- readability.c
+++ readability.c
@@ -1,10 +1,12 @@
 #include "./libs/cs50.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+
 
 int count_letters(string text);
 int count_words(string text, int start, int end);
+int count_sentences(string text);
 
 const int asciiOfDot = 46, asciiOfBlank = 32, asciiOfComma = 44, asciiOfQuestion = 63, asciiOfExclm = 33;
 
@@ -13,11 +15,33 @@ int main(void)
     // string text = "One fish. Two fish. Red fish. Blue fish.";
     string text = "Would you like them here or there? I would not like them here or there. I would not like them anywhere.";
     
-    
     int textLength = strlen(text);
-    int numberOfWords = count_words(text, 44, textLength);
+    int numberOfWords = count_words(text, 0, textLength);
     printf("%i\n", numberOfWords);
 
+    int numberOfSentencesss = count_sentences(text);
+    printf("%i\n", numberOfSentencesss);
+}
+
+int count_sentences(string text)
+{
+    int textLength = strlen(text);
+    int numberOfSentences = 0;
+    int j = 0;
+    for (int i = 0; i < textLength; i++)
+    {   
+        if ((int) text[i] == asciiOfDot || (int) text[i] == asciiOfQuestion || (int) text[i] == asciiOfExclm)
+        {
+            int numberOfWordsInThisSentence = count_words(text, j, i);
+            if (numberOfWordsInThisSentence > 0)
+            {
+                j = i + 1;
+                numberOfSentences++;            
+            }
+            j = i;
+        }
+    }
+    return numberOfSentences;
 }
 
 int count_words(string text, int start, int end)
